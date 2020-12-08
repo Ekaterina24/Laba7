3d384bcf797412b7337c910fdf9ec4222acad099
Ekaterina Rykova <katrykova2002@yandex.ru>
2020-12-08 13:08:12 +0300

    Task 1

---
+++ 1.js
@@ -0,0 +1,11 @@
+let a = 5;
+let st = "line";
+let bl = true;
+let n = null;
+
+console.log(a + st); //int + string == string
+console.log(a + bl); //int + boolean == int
+console.log(a + n); //int + null == int
+console.log(st + bl); //string + boolean == string
+console.log(st + n); //string + null = string
+console.log(bl + n); //boolean + null == int
