603e450f7ec129781f71bc4b448dcad4813b6531
Ekaterina Rykova <katrykova2002@yandex.ru>
2020-12-08 14:59:40 +0300

    Task 5

---
+++ 5.js
@@ -0,0 +1,8 @@
+console.log(`Задача 5`);
+let c1 = Number(prompt(`Первое число`));
+let c2 = Number(prompt(`Второе число`));
+
+let r = ((c1 + c2 == 10) || (c1 == 10) || (c2 == 10)) ? "true" : "false";
+console.log(r)
+
+
